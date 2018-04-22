41d5eae4ef2b04899aa2f402a97c057e646a15a8
Tiara Jarrett <tiarajarrett@tiaras-macbook-pro.local>
2018-04-21 23:55:17 -0500

    Initial Commit

---
+++ ListCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ListCoreData
+//
+//  Created by Tiara Jarrett on 4/21/18.
+//  Copyright © 2018 Tiara Jarrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
